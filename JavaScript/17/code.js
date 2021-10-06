9625ac0ae5910c3d545263be8f54153ff169ec40
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2021-10-06 16:21:01 -0300

    numero primo

--- JavaScript/17/code.js
+++ JavaScript/17/code.js
@@ -2,5 +2,18 @@ var numero = new Number(prompt("ingrese un numero"))
 
 
 var aPrimo = (num) => {
-    
+    if (num == 0 || num == 1 || num == 4) {
+        document.write(`${num} no es un numero primo`);
+        return false
+    };
+    for (let x = 2; x < num / 2; x++) {
+        if (num % x == 0) {
+            document.write(`${num} no es un numero primo`);
+            return false
+        }
+
+    }
+    document.write(`${num} es un numero primo`);
 }
+
+aPrimo(numero);
