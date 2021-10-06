582a2a625d63a6c5a5c64bc0f1e8b49d3059e57c
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2021-10-06 16:47:22 -0300

    ..

---
+++ JavaScript/18/code.js
@@ -0,0 +1,4 @@
+
+var edad = Number(prompt("cual es tu edad?: "))
+var clasificacion = edad > 18 ? document.write("usted SI es mayor de edad puede conducir"):document.write("usted NO es mayor de edad NO puede conducir");
+
