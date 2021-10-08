b74c76f595fba577abcd5593bfe30635b80aea6d
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2021-10-08 00:29:26 -0300

    carga

---
+++ JavaScript/21/code.js
@@ -0,0 +1,18 @@
+var suma=0;
+var run = true;
+while (run === true){
+    var num1 = prompt(`ingrese un numero o "X" para cancelar`);
+    var corte = "X";
+    if (num1 == corte || num1 == null){
+        var run = false;
+    }else if(num1 == null || Number.NaN(num1)){
+        alert("no ingreso un Numero");
+        num1 = Number(0);
+        var suma = suma + num1;            
+    }
+    else{
+        num1 = Number(num1)
+         var suma = suma + num1            
+    }
+}
+document.write(`la suma es ${suma}`)
