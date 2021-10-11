0ce998ec45a3fb6c3b4041ca22cb84aa050f7e6d
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2021-10-11 18:20:08 -0300

    ..

--- JavaScript/test/code.js
+++ JavaScript/test/code.js
@@ -1,42 +1,75 @@
 
-let obj = {a:1,b:2,c:3};
+// let obj = {a:1,b:2,c:3};
 
-for(let prop in obj){
-    console.log(`obj.${prop} = ${obj[prop]}`);
-}
-
-
-
-let iterable = [10, 20, 30];
-
-for (let value of iterable) { 
-    value += 1; console.log(value);
-}
+// for(let prop in obj){
+//     console.log(`obj.${prop} = ${obj[prop]}`);
+// }
 
 
-let iterable1 = "boo";
 
-for (let value of iterable1) { 
-    console.log(value);
-}
+// let iterable = [10, 20, 30];
+
+// for (let value of iterable) { 
+//     value += 1; console.log(value);
+// }
 
 
-let arr = [3, 5, 7]; arr.foo = "hola";
+// let iterable1 = "boo";
+
+// for (let value of iterable1) { 
+//     console.log(value);
+// }
+
+
+// let arr = [3, 5, 7]; arr.foo = "hola";
 
  
 
-for (let i in arr) {
+// for (let i in arr) {
 
-console.log(i); // logs "0", "1", "2", "foo"
+// console.log(i); // logs "0", "1", "2", "foo"
 
-}
-
-
+// }
 
 
-for (let i of arr) {
 
-console.log(i);;; // logs "3",, "5", "7"
 
-}
+// for (let i of arr) {
 
+// console.log(i);;; // logs "3",, "5", "7"
+
+// }
+
+
+
+// document.getElementsByTagName("p")[1].innerHTML= "Hello";
+ document.getElementsByTagName("p")[0].innerHTML= "Hello";
+//  document.getElementsByTagName("p").innerHTML= "Hello";
+
+// document.getElementById("image").source = "pic_mountain.jpg";
+ document.getElementById("image").src = "pic_mountain.jpg";
+//  document.getElementById("image").image= "pic_mountain.jpg";
+
+// document.getElementById("demo1").fontColor = "red";
+//  document.getElementById("demo1").color = "red";
+ document.getElementById("demo1").style.color = "red";
+
+
+
+ var person = {
+    name: "John",
+    age: 50
+  };
+
+//   alert(person.name + " is " +person.age );
+//   alert(person.name , " is " , person.age );
+//   alert(name + " is " + age );
+
+var frutas = ["Manzana", "Plátano", "Naranja"];
+
+
+for (i=0;i<3;i++) {
+
+    console.log(frutas[i]);
+  
+  }
--- JavaScript/unidad 1/22/code.js
+++ JavaScript/unidad 1/22/code.js
@@ -1,26 +1,29 @@
 
-var dni = Number(prompt("Ingrese su DNI"))
-
-var analisisDNI = (dni) => {
-
-    if (0 > dni <= 99999999) {
-
+var analisisDNI = () => {
+    let letraDni = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", 'X', "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
+    var run = true
+    while (run === true) {
+        let dni = Number(prompt("Ingrese su DNI"));
+        var rango = () => {
+            if (dni < 99999999 & dni > 0) {
+                alert("numero en de rango");
+                return true
+            }
+            else {
+                alert("numero fuera de rango");
+                return false
+            }
+        }
+        var rango = rango()
+        if (rango == true) {
+            datoDni = dni % 23;
+            document.write(`Su dni(${dni}) equivale a  la letra ${letraDni[datoDni]}`);
+            var run = false
+        }
+        else {
+            alert("Ingrese numeros");
+            var run = true
+        }
     }
-
-}
-function creaSumador(x) {
-
-    return function (y) {
-
-        return x + y;
-
-    };
-
-}
-var suma5 = creaSumador(5);
-
-var suma10 = creaSumador(10);
-
-console.log(suma5(2)); // muestra 7
-
-console.log(suma10(2)); // muestra 12
+};
+analisisDNI();
