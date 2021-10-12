3e07b6007f572e1897598cd494590868cf4421ef
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2021-10-12 15:59:05 -0300

    termidado 2

--- JavaScript/unidad 2/2/code.js
+++ JavaScript/unidad 2/2/code.js
@@ -1,1 +1,17 @@
-alert("Hello World")
+
+
+function maximo(num1 ,num2){
+    if(num1 === num2){
+        document.write(`num1:${num1} y num2:${num2} son exatamente iguales.`);
+    }
+    else if(num1 > num2){
+        document.write(`num1:${num1} es mayor que num2:${num2}`);
+    }else {
+        document.write(`num2:${num2} es mayor que num1:${num1}`);
+    }
+}
+
+var n1 = Number(prompt("ingrese un numero:"));
+var n2 = Number(prompt("ingrese un segundo numero:"));
+
+maximo(n1,n2);
