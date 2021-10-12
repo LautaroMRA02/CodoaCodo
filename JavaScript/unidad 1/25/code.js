5ce3e5bdfb6af8408b6b39335f6d2c4cfe4453a1
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2021-10-12 00:23:30 -0300

    terminado

--- JavaScript/unidad 1/25/code.js
+++ JavaScript/unidad 1/25/code.js
@@ -1,18 +1,17 @@
-
 var contador = 0;
-for(let numero = 1 ; numero <= 500; numero++){
-    if(numero % 4 == 0){
+for (let numero = 1; numero <= 500; numero++) {
+    if (numero % 4 == 0) {
         document.write(`${numero} (es multiplo de 4)<br>`);
     }
-    else if (numero % 9 == 0){
+    else if (numero % 9 == 0) {
         document.write(`${numero} (es multiplo de 9)<br>`);
-    }else{
+    } else {
         document.write(`${numero} <br>`)
     }
     contador++
-    if(contador === 5){
+    if (contador === 5) {
         var contador = 0;
         document.write(`--------------------------- <br>`)
-    }else{continue}
+    } else { continue }
 
-} 
+}
