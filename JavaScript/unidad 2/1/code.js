682f0c86f64983bd060061a9da8222d10e928e74
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2021-10-12 15:52:04 -0300

    terminado 1

--- JavaScript/unidad 2/1/code.js
+++ JavaScript/unidad 2/1/code.js
@@ -1,1 +1,9 @@
-alert("Hello World")
+
+var n = Number(prompt("ingrese un numero"));
+function saludo(n){
+    for (let x=0;x<n;x++){
+        console.log(`Bienvenidos al curso Full Stack`);
+    }
+}
+    
+saludo(n)
