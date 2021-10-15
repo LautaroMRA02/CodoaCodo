83d8c2d0ac54a572485eb990d3f978d1cf36574c
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2021-10-15 18:41:34 -0300

    getDataInput

--- JavaScript/vali-input/code.js
+++ JavaScript/vali-input/code.js
@@ -1,4 +1,8 @@
-var email = document.querySelectorAll('.need-validation');
 
-console.dir(email);
-console.log(email);
+const getValueInput = () =>{
+    let inputValue = document.getElementById("domTextElement").value; 
+    document.getElementById("valueInput").innerHTML = inputValue;
+
+    console.log(inputValue)
+  };
+  
