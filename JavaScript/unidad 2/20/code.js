fdec65dc75ef1422c59db6bd67a84572814b8fe5
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2021-10-17 23:56:48 -0300

    terminado

--- JavaScript/unidad 2/20/code.js
+++ JavaScript/unidad 2/20/code.js
@@ -1,1 +1,14 @@
-alert("Hello World")
+var edades = new Array(18,17, 17, 18, 20, 18, 18, 19, 16);
+
+document.write(`la cantidad de alumnos son: ${edades.length}<br>`)
+
+let i = 1;
+while(i < edades.length){
+    if(edades[i]%2==0){
+        document.write(edades[i],"<br>");
+        i++
+    } else {
+        i++
+    }
+}
+document.write(`estas son todas las edades ${edades}`);
